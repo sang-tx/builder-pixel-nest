6d46575ee150062b5b9879d8447055f5ec856c62
MD_Sang <sang.tx@mor.com.vn>
2025-08-19 13:11:37 +0700

    Update to landing page

---
+++ src/pages/Index.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import CTA from "@/components/CTA";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Hero />
+      <Features />
+      <CTA />
+    </div>
+  );
+};
+
+export default Index;
